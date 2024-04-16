803a3e4b5d7241a1f49b109017a3846ff93c2761
alienfoxx <alienfoxx001@gmail.com>
2024-04-16 23:34:19 +0100

    finishe the calculator logic app all is Done

--- Basic-Calculator/main.js
+++ Basic-Calculator/main.js
@@ -1,1 +1,32 @@
+const buttonsEl = document.querySelectorAll("button");
+const inputFieldEl = document.getElementById("result");
 
+for (let i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener("click", () => {
+    const buttonValue = buttonsEl[i].textContent;
+    if (buttonValue === "C") {
+      clearResult();
+    } else if (buttonValue === "=") {
+      calculateResult();
+    } else if (buttonValue === "R") {
+      removeValue();
+    } else {
+      appendValue(buttonValue);
+    }
+  });
+}
+function clearResult() {
+  inputFieldEl.value = "";
+}
+function calculateResult() {
+  inputFieldEl.value = eval(inputFieldEl.value);
+}
+
+function appendValue(buttonValue) {
+  inputFieldEl.value += buttonValue;
+}
+function removeValue() {
+  const newVal = inputFieldEl.value.toString().slice(0, -1);
+  clearResult();
+  appendValue(newVal);
+}
